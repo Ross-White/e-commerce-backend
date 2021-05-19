404012a2930127ec66801c09e4da083fa45a113e
Ross White <rosswhite@outlook.com>
2021-05-19 22:17:09 +0100

    Get category by ID working

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -8,7 +8,7 @@ router.get('/', async (req, res) => {
   // be sure to include its associated Products
   try {
     const categoryData = await Category.findAll({
-      include: [{model: Product}]
+      include: [{ model: Product }]
     });
     res.status(200).json(categoryData);
   } catch (err) {
@@ -19,10 +19,18 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
-  const categoryData = Category.findByPk(req.params.id).catch((err) => {
-    res.json(err);
-  });
-  res.json(categoryData);
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+    if (!categoryData) {
+      res.status(404), json({ message: 'No Category Found! ' });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
